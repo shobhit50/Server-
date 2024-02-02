2051e9d7f6e9fd16b8020326b22cd5dce3a55dcf
Shobhit kumar parashar <shobhitparashar0@gmail.com>
2024-02-02 17:36:46 +0530

    update

--- myfirstapp/Serializer.py
+++ myfirstapp/Serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Data , Webhook
+from bson import ObjectId
 
 class DataSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,7 @@ class WebhookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Webhook
         fields = '__all__'  
+    def create(self, validated_data):
+        # Convert the string representation of ObjectId to an ObjectId object
+        validated_data['id'] = ObjectId(validated_data['id'])
+        return Webhook.objects.create(**validated_data)
